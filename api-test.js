e8d0e4bdb00de97c8b8c954660385927b90c4d87
Spencer <spencer@artix.cc>
2017-01-04 01:06:58 -0500

    Make it work! Add methods, fix RTM stuff

--- api-test.js
+++ api-test.js
@@ -1,7 +1,19 @@
 let SlackAPI = require('./lib/SlackAPI');
 
 let slackBot = new SlackAPI('xoxb-47049102661-4j7PIjq3o6cdhXKgilfABeVL');
-slackBot.rtm.start();
+
+/*slackBot.methods.auth.test({}, (success, res)=>{
+    console.log(success);
+    console.log(JSON.stringify(res));
+});*/
+
+//slackBot.rtm.start();
+
+slackBot.methods.chat.postMessage({channel: 'chat', text: `Long text here`}, (success, res)=>{
+    console.log(success);
+    console.log(res);
+});
+
 slackBot.on('hello', ()=>{
-    console.log('hi');
+
 });
