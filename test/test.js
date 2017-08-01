2e147b384d518561ab6b612570b9642521268c6f
Spencer <spencer@artix.cc>
2017-08-01 17:06:55 -0400

    Add a currently non functional should throw test

--- test/test.js
+++ test/test.js
@@ -1,12 +1,23 @@
-const { assert } = require('chai');
+const chai = require('chai');
+const chaiAsPromised = require('chai-as-promised');
+chai.use(chaiAsPromised);
+const { assert } = chai;
+chai.should();
+
 const Client = require('../lib/client/Client');
 
 describe('Carabiner', function () {
     describe('Web API', function () {
         const client = new Client(process.env.SLACK_TOKEN);
 
+        it('should throw an error when slack does', async function () {
+            const nullClient = new Client('invalid slack token');
+
+            return nullClient.api.methods.api.test().should.be.rejected;
+        });
+
         it('should return an object when making slack requests', async function () {
-            assert.typeOf((await client.api.methods.api.test()), 'object');
+            return client.api.methods.api.test().should.eventually.be.an('object');
         });
     });
 });
