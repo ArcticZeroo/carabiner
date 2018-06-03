7348a71b5eb2e60914de17ced2d1149b8a9a6ca6
Spencer <spencer@frozor.io>
2018-06-03 17:25:55 -0400

    More styling to get in line with eslint

--- config/attachments.js
+++ config/attachments.js
@@ -1,4 +1,4 @@
 module.exports = {
-    attachment_properties: ['fallback', 'color', 'pretext', 'author_name', 'author_link', 'author_icon', 'title', 'title_link', 'text', 'image_url', 'thumb_url', 'footer', 'footer_icon', 'ts'],
-    field_properties: ['title', 'value', 'short']
+    attachmentProperties: ['fallback', 'color', 'pretext', 'author_name', 'author_link', 'author_icon', 'title', 'title_link', 'text', 'image_url', 'thumb_url', 'footer', 'footer_icon', 'ts'],
+    fieldProperties: ['title', 'value', 'short']
 };
